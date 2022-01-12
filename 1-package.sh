ae40586207993d6314a14c6dff4c628bb739aa56
Dita Aji Pratama <ditaajipratama@protonmail.com>
2022-01-12 10:12:36 +0700

    Create 1-package.sh

---
+++ 1-package.sh
@@ -0,0 +1,7 @@
+# flatpak
+sudo apt-get install -y flatpak
+sudo apt-get install -y gnome-software-plugin-flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# npm
+sudo apt-get install -y npm
