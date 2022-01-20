748ea4440e9bcb198967a475a3e830e9ea0ed677
Dita Aji Pratama <ditaajipratama@protonmail.com>
2022-01-20 10:12:59 +0700

    Adding meld

--- 3-programming.sh
+++ 3-programming.sh
@@ -1,2 +1,2 @@
-sudo apt-get install -y filezilla vim emacs glade fritzing
+sudo apt-get install -y filezilla vim emacs meld glade fritzing
 sudo flatpak install -y flathub io.github.gitahead.GitAhead org.kicad.KiCad
