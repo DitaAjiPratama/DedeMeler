6022b9b1c4458b318dee5e2447fca55efc1b9d76
Dita Aji Pratama <ditaajipratama@protonmail.com>
2022-01-20 10:16:37 +0700

    Adding FreeCAD

--- 3-programming.sh
+++ 3-programming.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y filezilla vim emacs meld glade fritzing
-sudo flatpak install -y flathub io.github.gitahead.GitAhead org.kicad.KiCad
+sudo flatpak install -y flathub io.github.gitahead.GitAhead org.kicad.KiCad org.freecadweb.FreeCAD
