60b5a91efbc998bc45a09e56c045947882b20bf2
Dita Aji Pratama <ditaajipratama@protonmail.com>
2022-02-16 18:49:53 +0700

    Update 7-media.sh

--- 7-media.sh
+++ 7-media.sh
@@ -1,2 +1,2 @@
-sudo apt-get install -y celluloid qbittorrent
+sudo apt-get install -y gimp celluloid qbittorrent
 sudo flatpak install -y flathub com.github.unrud.VideoDownloader com.adobe.Flash-Player-Projector org.kde.kdenlive
