f2f7be89ab5c3758ecf434d5309430773123d73c
Dita Aji Pratama <aji@debian-BULLSEYE-live-builder-AMD64>
2023-02-08 15:11:32 +0700

    Add another utilities

--- 2-utilities.sh
+++ 2-utilities.sh
@@ -1,2 +1,3 @@
 sudo apt-get install -y gparted neofetch
+sudo apt-get install -y rofi nitrogen volumeicon-alsa xcompmgr xclip thunar lxappearance lxappearance-obconf
 sudo apt-get install -y network-manager-openvpn network-manager-openvpn-gnome
