4ef563939b133dbc12ded7c2a238e770ee723377
Dita Aji Pratama <aji@debian-BULLSEYE-live-builder-AMD64>
2023-02-10 19:42:41 +0700

    xfce4-power-manager

--- 2-utilities.sh
+++ 2-utilities.sh
@@ -1,3 +1,3 @@
 sudo apt-get install -y gparted neofetch
-sudo apt-get install -y rofi nitrogen volumeicon-alsa xcompmgr xclip thunar lxappearance lxappearance-obconf
+sudo apt-get install -y rofi nitrogen volumeicon-alsa xcompmgr xclip thunar lxappearance lxappearance-obconf xfce4-power-manager
 sudo apt-get install -y network-manager-openvpn network-manager-openvpn-gnome
