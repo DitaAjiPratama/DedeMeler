5a8c14a3ec0b8ffe3ec6cffc99df3429626b9e4d
Dita Aji Pratama <aji@aji-dd>
2023-11-27 23:39:19 +0700

    Update 3-programming.sh

--- 3-programming.sh
+++ 3-programming.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y filezilla vim emacs meld glade fritzing
-sudo flatpak install -y flathub io.github.gitahead.GitAhead org.kicad.KiCad org.freecadweb.FreeCAD org.blender.Blender org.godotengine.Godot net.meshlab.MeshLab
+sudo flatpak install -y flathub com.github.Murmele.Gittyup org.kicad.KiCad org.freecadweb.FreeCAD org.blender.Blender org.godotengine.Godot net.meshlab.MeshLab
