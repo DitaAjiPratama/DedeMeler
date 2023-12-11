dec7d265ce00531e140a3c0b448c9c4d1b61f227
Dita Aji Pratama <aji@aji-dd>
2023-12-11 18:18:58 +0700

    Update 11-rice.sh: datetime settings

--- 11-rice.sh
+++ 11-rice.sh
@@ -1,1 +1,6 @@
 sudo apt-get install -y openbox lxpolkit kitty rofi tint2 thunar thunar-archive-plugin
+
+sudo timedatectl set-timezone Asia/Jakarta
+# sudo dpkg-reconfigure tzdata
+sudo apt-get install -y ntpdate
+sudo ntpdate pool.ntp.org
